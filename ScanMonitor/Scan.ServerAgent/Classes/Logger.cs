a141b3bcc54998901ae43a6e83690008baabbca7
Jayashan Liyanage <laksrijayashan@gmail.com>
2020-04-05 10:02:00 +0530

    Added logger to ServerAgent

---
+++ ScanMonitor/Scan.ServerAgent/Classes/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scan.ServerAgent.Classes
+{
+    public static class Logger
+    {
+        public static void WriteLine(string message)
+        {
+            message = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt") + "\t" + message;
+
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
